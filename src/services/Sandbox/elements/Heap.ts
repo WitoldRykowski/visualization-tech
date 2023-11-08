280f2b98b981df709b1934fd4b5b1ce2a4e64c9f
WitoldRykowski <rykowskiwitold@gmail.com>
2023-11-08 21:59:52 +0100

    Fix binary heap nodes order

--- src/services/Sandbox/elements/Heap.ts
+++ src/services/Sandbox/elements/Heap.ts
@@ -9,13 +9,13 @@ export const Heap = (values: number[]): HeapInstance => {
     draw
   }
 
-  const { width } = getSandboxSize()
-
-  const queue = []
-  const root = Point({ x: width / 2 - 10, y: 50, value: values[0] })
+  const { width, height } = getSandboxSize()
+  const SPACING_Y = 50
+  const queue: Point[] = []
+  const valuesCopy = [...values]
+  const root = Point({ x: width / 2 - 10, y: SPACING_Y, value: valuesCopy.shift() })
 
   heap.points.push(root)
-  // TODO Fix the order
   queue.push(root)
   const levels = Math.ceil(Math.log2(values.length + 1))
 
@@ -23,30 +23,37 @@ export const Heap = (values: number[]): HeapInstance => {
     const nodesAtLevel = 2 ** level
     const xIncrease = width / 2 / nodesAtLevel
 
+    // if (valuesCopy.length < nodesAtLevel) {
+    //   nodesAtLevel = valuesCopy.length
+    // }
+
     for (let node = 1; node < nodesAtLevel; node += 2) {
       const parent = queue.shift()!
+      const left = addNode(parent, -xIncrease)
 
-      const left = Point({ x: parent.x - xIncrease, y: parent.y + 50 })
-      const right = Point({ x: parent.x + xIncrease, y: parent.y + 50 })
-
-      heap.points.push(left)
-      const leftConnection = Connection({ startAt: parent, finishAt: left })
-      left.connections.push(leftConnection)
-      heap.connections.push(leftConnection)
-      if (heap.points.length === values.length) break
-
-      heap.points.push(right)
-      const rightConnection = Connection({ startAt: parent, finishAt: right })
-      right.connections.push(rightConnection)
-      heap.connections.push(rightConnection)
-      if (heap.points.length === values.length) break
-
-      queue.push(right)
       queue.push(left)
+      if (!valuesCopy.length) break
+
+      const right = addNode(parent, xIncrease)
+      queue.push(right)
     }
   }
 
-  console.log(heap.points)
+  function addNode(parent: Point, xModifier: number) {
+    const levelsSpacing = (height - 2 * SPACING_Y) / levels
+    const node = Point({
+      x: parent.x + xModifier,
+      y: parent.y + levelsSpacing,
+      value: valuesCopy.shift()
+    })
+
+    heap.points.push(node)
+    const leftConnection = Connection({ startAt: parent, finishAt: node })
+    node.connections.push(leftConnection)
+    heap.connections.push(leftConnection)
+
+    return node
+  }
 
   draw()
 
@@ -60,7 +67,7 @@ export const Heap = (values: number[]): HeapInstance => {
     }
 
     for (const point of heap.points) {
-      isChanged = point.draw() || isChanged
+      isChanged = point.draw(30) || isChanged
     }
 
     return isChanged
--- src/services/heap-sort.service.ts
+++ src/services/heap-sort.service.ts
@@ -1,21 +1,17 @@
-import type { Column } from '@/services/Sandbox/elements/Column'
 import { initAnimation } from '@/services/Sandbox/sandbox.service'
 import { generateNonSortedArray } from '@/services/Array/array.service'
-import { getColumns } from '@/services/Sandbox/Creator'
 import type { VariantSetup } from '@/services/Sandbox/types'
 import { Heap, type HeapInstance } from '@/services/Sandbox/elements/Heap'
 
 let moves: Move[] = []
 let values: number[] = []
-let columns: Column[] = []
 let heap: HeapInstance | undefined = undefined
 
 const initHeapSort = () => {
   initAnimation(init, animateHeapSort)
 
   function init() {
-    values = generateNonSortedArray(10)
-    columns = getColumns(values)
+    values = generateNonSortedArray(30)
     moves = []
   }
 
